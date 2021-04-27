0e9cfe437906d4db82efddc2c10a6785dd4650af
Raghav Sharma <raghav_tinker@yahoo.com>
2021-04-28 01:05:24 +0530

    Step 1 of swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+import PlaygroundSupport
+
+print("Hello World")
+
+//Variables
+var myVariable: String = "Raghav is a human"
+let myConstant: String = "Raghav is an organism"
+myVariable = "Raghav is not a human"
+let myDouble: Double = 12.0
+//You can also create variables without mentioning the
+var variable = 12
+//Concatenation of strings
+let label = "the width is "
+let width = 34
+let widthLabel = label + String(width)
+//Or
+let summary = "The width is \(width)"
+//Multi line string
+let some_string = """
+    I am raghav
+    I am a human
+"""
+
+
+
