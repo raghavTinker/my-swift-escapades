f7b2f270a11e472897e01789fd9f93c0328ca268
Raghav Sharma <raghav_tinker@yahoo.com>
2021-04-30 19:25:10 +0530

    Some more examples

--- MyPlayground.playground/Pages/CollectionOfItems.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/CollectionOfItems.xcplaygroundpage/Contents.swift
@@ -10,12 +10,12 @@ print(menu)
 var emptyArray = [String]()
 
 //Dictionaries
-var myDict = [String: String]()
-myDict["LOL"] = "Laugh out loud"
-myDict["LMAO"] = "Laugh my ass off"
-print(myDict)
-print(myDict["LOL"] == nil)
-var searchLOL: String? = myDict["LOL"]
+var myDictNew = [String: String]()
+myDictNew["LOL"] = "Laugh out loud"
+myDictNew["LMAO"] = "Laugh my ass off"
+print(myDictNew)
+print(myDictNew["LOL"] == nil)
+var searchLOL: String? = myDictNew["LOL"]
 
 //Optional unwrapping next module
 if let someThing = searchLOL{
@@ -24,3 +24,29 @@ if let someThing = searchLOL{
 else{
     print("LOL")
 }
+
+var myDict = [String:Int]()
+
+myDict["raghav"] = 19
+myDict["medha"] = 17
+
+for(key, value) in myDict{
+    print("\(key) = \(value)")
+}
+
+var myArray: [String] = ["Dog", "cat", "bird"]
+
+myArray[0] = "My " +     myArray[0]
+
+print(myArray)
+
+//Size of array
+print(myArray.count)
+
+//append
+myArray.append("Raghav")
+
+//insert
+myArray.insert("Hello", at: 3)
+print(myArray)
+
